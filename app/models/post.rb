e4dee2c5f6d0e6822ac9c90414856ac743458279
jyk595 <jyk595@gmail.com>
2021-08-31 09:39:34 -0700

    commit tree 69eb97d34dc45d2df57f571b8e049559aa97d8a3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validate :title_validator, on: :title
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait?
+
+  BAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+  def clickbait?
+    if BAIT_PATTERNS.none? { |pattern| pattern.match(title) }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
